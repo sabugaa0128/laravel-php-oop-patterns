ccf646a7b703d36bc61c4301ba16f0ae5b63e157
sabugaa0128 <sabugaarozvelt@gmail.com>
2024-03-28 02:53:55 +0200

    Created Factory pattern

---
+++ app/DesignPatterns/Creational/Factory/Worker.php
@@ -0,0 +1,21 @@
+<?php
+
+class Worker {
+    private string $name;
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+}
